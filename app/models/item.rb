84f4dfa8899a7dca6d4da10c54d618311de19446
bhishmaLamichhane <itbhishma@outlook.com>
2016-09-13 00:39:48 +0545

    prepare controller

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,13 @@
 class Item < ApplicationRecord 
+  attr_accessor :document_data
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png" 
   validates_attachment :picture, presence: true 
   do_not_validate_attachment_file_type :picture 
   has_many :documents
+
+  def save_attachments(params) 
+    params[:document_data].each do |doc| 
+      self.documents.create(:file => doc) 
+    end 
+  end
 end
