c8e6c83f4ca32172357a7ed4d796ce29d877349a
bhishmaLamichhane <itbhishma@outlook.com>
2016-09-13 00:28:03 +0545

    Add document model for supporting multiple file upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912174954) do
+ActiveRecord::Schema.define(version: 20160912183740) do
+
+  create_table "documents", force: :cascade do |t|
+    t.integer  "item_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
+    t.index ["item_id"], name: "index_documents_on_item_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
